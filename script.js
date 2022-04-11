43c2c4882774b9dc777c1b41fe83bd45923753af
arifizzu <88992071+arifizzu@users.noreply.github.com>
2022-04-12 02:03:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ var name1=prompt("Enter Your Name");
  var name2=prompt("What Did I Call You");
  var a = name1.toLowerCase();
  var b = name2.toLowerCase();
-  	if( (a== "dalila" || a== "diyana" || a== "dalila diyana" || a== "dalila diyana binti yusdi" || a== "dalila diyana bt yusdi" || a== "del") 
-	  && (b=="diyana" || b=="yoda" || b=="yodalila" || b=="kak dila" || b=="del") )
+  	if(  a== "dalila diyana && b=="diyana" )
   	{
   	    window.location.href="birthday.html";
   	}
